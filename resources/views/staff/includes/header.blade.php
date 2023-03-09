1798a1c62dc7034330d3caf16272ef17d1c9d716
xenabyte <iconcept573@gmail.com>
2023-03-09 09:13:58 +0100

    remove previous attendance

--- resources/views/staff/includes/header.blade.php
+++ resources/views/staff/includes/header.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-        
+
         <meta charset="utf-8" />
         <title>Dashboard | {{ env('APP_NAME')}} - Staff </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,9 +17,9 @@
 
          <!-- Plugins css -->
         <link href="{{asset('assets/libs/dropzone/min/dropzone.min.css')}}" rel="stylesheet" type="text/css" />
- 
+
          <!-- Responsive datatable examples -->
-         <link href="{{asset('assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />     
+         <link href="{{asset('assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
 
         <!-- Bootstrap Css -->
         <link href="{{asset('assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
@@ -39,7 +39,7 @@
         <!-- Begin page -->
         <div id="layout-wrapper">
 
-            
+
             <header id="page-topbar">
                 <div class="navbar-header">
                     <div class="d-flex">
@@ -68,7 +68,7 @@
                             <i class="fa fa-fw fa-bars"></i>
                         </button>
 
-                       
+
                     </div>
 
                     <div class="d-flex">
@@ -116,13 +116,6 @@
                             </li>
 
                             <li>
-                                <a href="{{ url('/staff/attendance') }}" class="waves-effect">
-                                    <i class="bx bx-time-five"></i>
-                                    <span key="t-dashboards">Attendance</span>
-                                </a>
-                            </li>
-
-                            <li>
                                 <a href="{{ url('/staff/leaveDays') }}" class="waves-effect">
                                     <i class="mdi mdi-calendar-end"></i>
                                     <span key="t-dashboards">Leave Days</span>
@@ -136,7 +129,7 @@
                                 </a>
                             </li>
 
-                            
+
 
                         </ul>
                     </div>
@@ -145,7 +138,7 @@
             </div>
             <!-- Left Sidebar End -->
 
-            
+
 
             <!-- ============================================================== -->
             <!-- Start right Content here -->
@@ -155,7 +148,6 @@
                 <div class="page-content">
                     <div class="container-fluid">
 
-                        
-                 
-                
-               
+
+
+
